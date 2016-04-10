afcef4750ab281ab7157efcb171a0fa332fa84f6
Benjamin Lupton <b@lupton.cc>
2016-04-10 15:37:49 +0800

    Add reverse transform

--- node/handsonnode/~examples/03-server-simple/server-echo-transform.js
+++ node/handsonnode/~examples/03-server-simple/server-echo-transform.js
@@ -1,10 +1,18 @@
-class UC extends require('streams').Transform {
+'use strict'
+
+class UC extends require('stream').Transform {
 	_transform (data, encoding, next) {
-		next(null, data.toString().toUpperCase()
+		next(null, data.toString().toUpperCase())
+	}
+}
+
+class Reverse extends require('stream').Transform {
+	_transform (data, encoding, next) {
+		next(null, data.toString().split('').reverse().join(''))
 	}
 }
 
 require('net').createServer(function (socket) {
 	socket.write('Talk to me.\n')
-	socket.pipe(new UC()).pipe(socket)
+	socket.pipe(new UC()).pipe(new Reverse()).pipe(socket)
 }).listen(8000)
